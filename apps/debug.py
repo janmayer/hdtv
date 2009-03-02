450e56283951eb7de9e56bb2e64b62af0f600a8d
Tanja Kotthaus <t.kotthaus@ikp.uni-koeln.de>
2009-03-02 23:40:26 +0100

    * moved some old files to attic * seperated my debug script to an extra file

---
+++ apps/debug.py
@@ -0,0 +1,4 @@
+# for debugging
+#s.LoadSpectra(hdtvpath+"/test/spectra/231Th_7down.ascii")
+s.LoadSpectra(hdtvpath+"/test/spectra/231Th*.ascii")
+#s.ReadCalibrationList(hdtvpath+"/test/spectra/231Th_calibrations.txt")
