d74f57aa3a7d61f2a5d2eeb7272b68c95f069ebe
Tanja Kotthaus <t.kotthaus@ikp.uni-koeln.de>
2009-03-17 19:22:15 +0100

    Added some files to demonstrate a problem with the fitter.

---
+++ test/fit_problem/fit_test.py
@@ -0,0 +1,24 @@
+from hdtv.fit import Fit
+from hdtv.spectrum import FileSpectrum, SpectrumCompound
+
+
+### start!
+spectra.RemoveAll()
+
+fspec = FileSpectrum(hdtvpath+"/test/fit_problem/231Th_10down.ascii")
+spec = SpectrumCompound(window.viewport, fspec)
+spectra.Add(spec)
+
+fit = Fit(f.defaultFitter.Copy(), cal=spec.cal, color=spec.color)
+fit.PutRegionMarker(490)
+fit.PutRegionMarker(540)
+fit.PutPeakMarker(500)
+fit.PutPeakMarker(525)
+fit.FitPeakFunc(spec)
+
+spec.Add(fit)
+
+spectra.Draw(window.viewport)
+
+
+
