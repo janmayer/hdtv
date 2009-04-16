8c91b9eedd910d67d9ff7904293a9897cb1f1c71
Tanja Kotthaus <t.kotthaus@ikp.uni-koeln.de>
2009-04-17 00:02:13 +0200

    * added prettyprinting to fitlist XML plugin

--- hdtv/plugins/fitXML.py
+++ hdtv/plugins/fitXML.py
@@ -1,6 +1,6 @@
 import xml.etree.ElementTree as ET
 
-def WriteFitlist(spectra, filename=None):
+def WriteFitlist(spectra, filename=None, prettyprint=True):
 	root = ET.Element("hdtv")
 	for spec in spectra.itervalues():
 		specXML = ET.SubElement(root, "spectrum")
@@ -16,26 +16,26 @@ def WriteFitlist(spectra, filename=None):
 			for marker in fit.bgMarkers:
 				markerXML = ET.SubElement(fitXML, "background")
 				p1XML = ET.SubElement(markerXML, "p1")
-				p1XML.set("calibrated", str(marker.p1))
+				p1XML.set("calibrated", "%.3f" %marker.p1)
 				p1XML.text = str(spec.cal.E2Ch(marker.p1))
 				p2XML = ET.SubElement(markerXML, "p2")
-				p2XML.set("calibrated", str(marker.p2))
+				p2XML.set("calibrated", "%.3f" %marker.p2)
 				p2XML.text = str(spec.cal.E2Ch(marker.p2))
 			for marker in fit.regionMarkers:
 				markerXML = ET.SubElement(fitXML, "region")
 				p1XML = ET.SubElement(markerXML, "p1")
-				p1XML.set("calibrated", str(marker.p1))
+				p1XML.set("calibrated", "%.3f" %marker.p1)
 				p1XML.text = str(spec.cal.E2Ch(marker.p1))
 				p2XML = ET.SubElement(markerXML, "p2")
-				p2XML.set("calibrated", str(marker.p2))
+				p2XML.set("calibrated", "%.3f" %marker.p2)
 				p2XML.text = str(spec.cal.E2Ch(marker.p2))
 			for marker in fit.peakMarkers:
 				markerXML = ET.SubElement(fitXML, "peaks")
 				p1XML = ET.SubElement(markerXML, "p1")
-				p1XML.set("calibrated", str(marker.p1))
+				p1XML.set("calibrated", "%.3f" %marker.p1)
 				p1XML.text = str(spec.cal.E2Ch(marker.p1))
 			for result in fit.fitter.GetResults():
-				resultXML = ET.SubElement(fitXML, "result")
+				resultXML = ET.SubElement(fitXML, "results")
 				for param in fit.fitter.fParStatus.iterkeys():
 					paramXML = ET.SubElement(resultXML, param)
 					status = fit.fitter.fParStatus[param]
@@ -46,11 +46,13 @@ def WriteFitlist(spectra, filename=None):
 						valueXML.text = str(param.value)
 						errorXML = ET.SubElement(paramXML, "error")
 						errorXML.text = str(param.error)
-	tree = ET.ElementTree(root)
+	if prettyprint:
+		indent(root)
 	if filename:
+		tree = ET.ElementTree(root)
 		tree.write(filename)
 	else:
-		ET.dump(tree)
+		ET.dump(root)
 
 		
 def ReadFitlist(filename):
@@ -58,6 +60,25 @@ def ReadFitlist(filename):
 	root = tree.getroot()
 	for spec in root.getiterator():
 		pass
+
+def indent(elem, level=0):
+	"""
+	in-place prettyprint formatter
+	from: http://effbot.org/zone/element-lib.htm#prettyprint
+	"""
+	i = "\n" + level*"  "
+	if len(elem):
+		if not elem.text or not elem.text.strip():
+			elem.text = i + "  "
+		if not elem.tail or not elem.tail.strip():
+			elem.tail = i
+		for elem in elem:
+			indent(elem, level+1)
+		if not elem.tail or not elem.tail.strip():
+			elem.tail = i
+	else:
+		if level and (not elem.tail or not elem.tail.strip()):
+			elem.tail = i
 
 
 print "loaded fitXML plugin"
