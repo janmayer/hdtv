34f79e0abd3398917c9de6ed130ce56b37d1034d
Ralf Schulze <ralf.schulze@ikp.uni-koeln.de>
2009-08-06 10:41:52 +0200

    Changed "ID" for gammas in library entries.
     * now e.g. 63-Cu@183

--- hdtv/database/common.py
+++ hdtv/database/common.py
@@ -196,7 +196,7 @@ class Gamma(object):
     __slots__ = ("id", "nuclide", "energy", "sigma", "intensity")
      
     def __init__(self, nuclide, energy, sigma, intensity):
-        self.id = str(nuclide.id) + "@" + str(energy)
+        self.id = str(nuclide.id) + "@" + str(energy.value.__int__())
         self.nuclide = nuclide
         self.energy = energy
         self.sigma = sigma
