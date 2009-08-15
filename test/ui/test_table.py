aabe5aec2f3bc022b486343615f459b389a4a47d
Ralf Schulze <ralf.schulze@ikp.uni-koeln.de>
2009-08-15 03:28:03 +0200

    Added table output to hdtv.ui

---
+++ test/ui/test_table.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from hdtv.ui import *
+import hdtv.database
+
+db = hdtv.database.PGAALibraries.PGAAlib_IKI2000()
+
+output = db.find(energy = 511, sort_key = "k0", fuzziness = 2)
+
+table = Table(output, header = ["ID", "Energy / (keV)", "Intensity / (%)", "Sigma / (b)"],
+              attrs = ["id", "energy", "intensity", "sigma"],
+              sortBy = "intensity", reverseSort = True)
+
+table.out()
+
+
+
+table2 = Table(output, attrs = ["id", "energy", "intensity", "sigma"])
+print table2
+
+
