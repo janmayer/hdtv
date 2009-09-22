c0473bc35bc638f1b4c6069cccc282f5b837e59b
Tanja Kotthaus <t.kotthaus@ikp.uni-koeln.de>
2009-09-22 16:07:08 +0200

    small fix to peakfinder

--- hdtv/plugins/peakfinder.py
+++ hdtv/plugins/peakfinder.py
@@ -182,7 +182,7 @@ class PeakFinder:
              
              if not reject:
                  ID = self.spectra[sid].fits.Add(fit)
-                 fit.SetTitle(str(ID) + "(*)")
+                 fit.title= str(ID) + "(*)"
         
         hdtv.ui.msg("Found " + str(num_peaks) + " peaks")
         return True
