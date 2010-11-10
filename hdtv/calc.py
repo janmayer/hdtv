47ac99fb8d3a3ea7687e8e1d85c2418c45181844
Ralf Schulze <ralf.schulze@ikp.uni-koeln.de>
2010-11-10 16:30:19 +0100

    Added forgotten math import to calc.py

--- hdtv/calc.py
+++ hdtv/calc.py
@@ -19,6 +19,8 @@
 # along with HDTV; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
 
+import math
+
 def Median(values):
     """
     Calculate Median of a list
