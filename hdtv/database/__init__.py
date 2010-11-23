d22e50179d368e8a9dcee50b980a05c27b972802
Ralf Schulze <ralf.schulze@ikp.uni-koeln.de>
2010-11-23 17:02:23 +0100

    Addes Elements() class to databases __init__.py

--- hdtv/database/__init__.py
+++ hdtv/database/__init__.py
@@ -1,3 +1,3 @@
-from PGAALibraries import PromptGammas, PGAAlib_IKI2000
+from PGAALibraries import PromptGammas, PGAAlib_IKI2000, Elements
 
 databases = {"promptgammas": PromptGammas, "pgaalib_iki2000": PGAAlib_IKI2000}
