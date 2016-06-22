7441301a930c7c390bcbb55d545a5dde458a49f8
Nima Saed-Samii <nima@ikp.uni-koeln.de>
2016-06-22 20:04:25 +0200

    Fix system-wide install when using ROOT6
    ROOT6 changed the interpreter to use LLVM.
    This change requires the header files to be available
    when loading shared libraries containing ROOT-dictionaries
    created by rootcint/rootcling. Also rootcint/rootcling
    now generates additional .pcm module files which have to
    be installed besides the .so file.
    
    This commit installs the required header files
    in $PREFIX/share/hdtv/include and adds that path
    to the ROOT_INCLUDE_PATH environment variable, which
    controlls which directories are searched.
    
    Install includes to $PREFIX/share/hdtv/include and add to ROOT_INCLUDE_PATH

--- setup.py
+++ setup.py
@@ -18,22 +18,27 @@ subprocess.call(["make", "-C", "src/display", "rootdict-display.cxx"])
 subprocess.call(["make", "-C", "src/fit", "rootdict-fit.cxx"])
 subprocess.call(["make", "-C", "src/mfile-root", "rootdict-mfile-root.cxx"])
 
-display = Extension('display', 
-                    sources = glob.glob('src/display/*.cxx'), 
+subprocess.call(['mkdir', '-p', 'hdtv/clib'])
+subprocess.call(['cp', 'src/display/rootdict-display_rdict.pcm', 'hdtv/clib/'])
+subprocess.call(['cp', 'src/fit/rootdict-fit_rdict.pcm', 'hdtv/clib/'])
+subprocess.call(['cp', 'src/mfile-root/rootdict-mfile-root_rdict.pcm', 'hdtv/clib/'])
+
+display = Extension('display',
+                    sources = glob.glob('src/display/*.cxx'),
                     include_dirs = [root_incdir],
                     depends = '',
 		    extra_link_args = [] + root_ldflags,
 		    extra_compile_args = [] + root_compile_args
                     )
-fit     = Extension('fit', 
-                    sources=glob.glob('src/fit/*.cxx'), 
+fit     = Extension('fit',
+                    sources=glob.glob('src/fit/*.cxx'),
                     include_dirs = [root_incdir],
                     depends='',
 		    extra_link_args = [] + root_ldflags,
 		    extra_compile_args = [] + root_compile_args
                     )
-mfile_root = Extension('mfile-root', 
-                    sources=glob.glob('src/mfile-root/*.cxx'), 
+mfile_root = Extension('mfile-root',
+                    sources=glob.glob('src/mfile-root/*.cxx'),
                     include_dirs = [root_incdir],
                     libraries=['mfile', 'stdc++'],
                     depends ='',
@@ -42,6 +47,19 @@ mfile_root = Extension('mfile-root',
                     )
 
 data = glob.glob('hdtv/share/*')
+sys_includes = ['src/display/Calibration.h','src/display/DisplayBlock.h',
+     'src/display/DisplayCut.h', 'src/display/DisplayFunc.h',
+     'src/display/DisplayObj.h', 'src/display/DisplaySpec.h',
+     'src/display/DisplayStack.h', 'src/display/Marker.h',
+     'src/display/MTViewer.h', 'src/display/Painter.h',
+     'src/display/View1D.h', 'src/display/View2D.h',
+     'src/display/Viewer.h', 'src/display/View.h',
+     'src/display/XMarker.h', 'src/display/YMarker.h',
+     'src/fit/Background.h', 'src/fit/EEFitter.h',
+     'src/fit/Fitter.h', 'src/fit/Integral.h',
+     'src/fit/Param.h', 'src/fit/PolyBg.h',
+     'src/fit/TheuerkaufFitter.h', 'src/mfile-root/MatOp.h',
+     'src/mfile-root/MFileHist.h', 'src/mfile-root/VMatrix.h']
 
 setup(name='hdtv',
     version = '0.1',
@@ -50,8 +68,9 @@ setup(name='hdtv',
     long_description=open('./README','r').read(),
     scripts = ['bin/hdtv'],
     packages=['hdtv', 'hdtv.plugins', 'hdtv.peakmodels', 'hdtv.efficiency', 'hdtv.database'],
-    package_data={'hdtv':['share/*']},
+    package_data={'hdtv':['share/*', 'clib/*.pcm']},
     ext_package='hdtv/clib',
-    ext_modules=[display, fit, mfile_root ]
+    ext_modules=[display, fit, mfile_root ],
+    data_files=[('share/hdtv/include', sys_includes)]
     )
 
