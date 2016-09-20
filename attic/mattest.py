4bb943447bc179dc9d662bdf1a406828eb53377d
Jan Mayer <jan.mayer@ikp.uni-koeln.de>
2016-09-20 10:39:15 +0200

    Delete the attic

--- attic/mattest.py
+++
@@ -1,9 +0,0 @@
-import ROOT
-from specreader import SpecReader
-
-if ROOT.gSystem.Load("mattest.so") < 0:
-	raise RuntimeError, "Library not found (mattest.so)"
-	
-mat = SpecReader().GetMatrix("/home/braun/Diplom/full_test/mat/all/all.mtx", "all.mtx", "all.mtx")
-	
-viewer = ROOT.MTViewer(400, 400, mat, "mattest")
