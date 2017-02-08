f58c92d342863521cc7bc31965196b3004325ca4
Nima Saed-Samii <nima@ikp.uni-koeln.de>
2017-02-08 16:49:00 +0200

    setup.py: sys_includes is broken
    Signed-off-by: Jan Mayer <jan.mayer@ikp.uni-koeln.de>

--- setup.py
+++ setup.py
@@ -9,13 +9,20 @@ from distutils.core import setup, Extension
 sys_includes = []
 print('Building C++ library display.so ...')
 subprocess.call(['make', '-C', 'src/display'])
-sys_includes += glob.glob('src/display/[!LinkDef]*.h')
+sys_includes += glob.glob('src/display/*.h')
 print('Building C++ library fit.so ...')
 subprocess.call(['make', '-C', 'src/fit'])
-sys_includes += glob.glob('src/fit/[!LinkDef]*.h')
+sys_includes += glob.glob('src/fit/*.h')
 print('Building C++ library mfile-root.so ...')
 subprocess.call(['make', '-C', 'src/mfile-root'])
-sys_includes += glob.glob('src/mfile-root/[!LinkDef]*.h')
+sys_includes += glob.glob('src/mfile-root/*.h')
+
+# Remove rootcling LinkDef.h from list of headers to
+# distribute
+sys_includes.remove('src/display/LinkDef.h');
+sys_includes.remove('src/fit/LinkDef.h');
+sys_includes.remove('src/mfile-root/LinkDef.h');
+
 
 import hdtv.version
 setup(name='hdtv',
