58987ac089b042f67d393daaa01a0b04e5d2a917
Udo Gayer <gayer.udo@gmail.com>
2019-06-26 07:22:11 +0200

    Further integration of background class. Removed unnecessary code.

--- hdtv/backgroundmodels/polynomial.py
+++ hdtv/backgroundmodels/polynomial.py
@@ -19,8 +19,6 @@
 # along with HDTV; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
 
-#import math
-
 import ROOT
 from .background import BackgroundModel
 
