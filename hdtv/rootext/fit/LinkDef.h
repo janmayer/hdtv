99e312cd64ce85da545601f327b49fdf041372b6
Udo Gayer <gayer.udo@gmail.com>
2019-06-26 16:10:23 +0200

    Towards a background interpolation. New InterpolationBg class.

--- hdtv/rootext/fit/LinkDef.h
+++ hdtv/rootext/fit/LinkDef.h
@@ -12,6 +12,7 @@
 #pragma link C++ class HDTV::Fit::TH1BgsubIntegral+;
 #pragma link C++ class HDTV::Fit::Background+;
 #pragma link C++ class HDTV::Fit::PolyBg+;
+#pragma link C++ class HDTV::Fit::InterpolationBg+;
 #pragma link C++ class HDTV::Fit::Param+;
 #pragma link C++ class HDTV::Fit::Fitter+;
 #pragma link C++ class HDTV::Fit::TheuerkaufPeak+;
