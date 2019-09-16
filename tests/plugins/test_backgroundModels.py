69f3ab5fd2330f2990708919e454da209a571f49
Udo Gayer <gayer.udo@gmail.com>
2019-09-16 09:01:45 +0200

    Test spectrum as temporary file
    - ... instead of storing it in test/share/
    - Also, do not store the corresponding hdtv scripts any more
    - Added license notices to test modules

--- tests/plugins/test_backgroundModels.py
+++ tests/plugins/test_backgroundModels.py
@@ -1,3 +1,22 @@
+# HDTV - A ROOT-based spectrum analysis software
+#  Copyright (C) 2006-2019  The HDTV development team (see file AUTHORS)
+#
+# This file is part of HDTV.
+#
+# HDTV is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version.
+#
+# HDTV is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with HDTV; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+
 """
 Test the different implemented background models with an artificial spectrum.
 The test spectrum is a sequence of step functions with increasing step height. On each step, a normal-distributed peak is located. No artificial fluctuations are introduced to give simple verifiable results.
@@ -11,7 +30,7 @@ import xml.etree.ElementTree as ET
 
 from test.helpers.utils import hdtvcmd, isclose
 from test.helpers.fixtures import temp_file
-from test.helpers.create_test_spectrum import ArtificialSpec
+from test.helpers.create_test_spectrum import ArtificialSpec, ArtificialSpecProp
 
 from numpy import exp, log, sqrt
 
@@ -41,9 +60,16 @@ N_SIGMA = 2 # This number determines how many standard deviations (sigma) a fitt
 # statistical considerations here.
 # For the former case, this test is self-consistent and assumes that the algorithm estimates the
 # uncertainty correctly.
+WRITE_BATCHFILE = False # Determines whether the batch files of the test fits should be written to file.
 
-ts = ArtificialSpec()
-ts.create()
+# Initialize the test spectrum properties which are needed for the parameterized tests
+ts_prop = ArtificialSpecProp()
+
+@pytest.fixture
+def test_spectrum(tmp_path):
+    ts = ArtificialSpec(path=tmp_path)
+    ts.create()
+    return ts
 
 @pytest.mark.parametrize(
         # nparams is the true number of parameters. The length n of the list bgparams
@@ -62,33 +88,31 @@ ts.create()
         ("polynomial", 4, 0, 4, "fit parameter background free", "", [10., 0.], 0.),
         ("interpolation", 6, 0, 2, "", "Background fit failed.", [], 0.),
         ("interpolation", 6, 0, 3, "", "", [
-            0.5*(ts.bg_regions[0][0]+ts.bg_regions[0][1])*ts.nbins_per_step, ts.bg_per_bin,
-            0.5*(ts.bg_regions[1][0]+ts.bg_regions[1][1])*ts.nbins_per_step, ts.bg_per_bin,
-            0.5*(ts.bg_regions[2][0]+ts.bg_regions[2][1])*ts.nbins_per_step, ts.bg_per_bin
+            0.5*(ts_prop.bg_regions[0][0]+ts_prop.bg_regions[0][1])*ts_prop.nbins_per_step, ts_prop.bg_per_bin,
+            0.5*(ts_prop.bg_regions[1][0]+ts_prop.bg_regions[1][1])*ts_prop.nbins_per_step, ts_prop.bg_per_bin,
+            0.5*(ts_prop.bg_regions[2][0]+ts_prop.bg_regions[2][1])*ts_prop.nbins_per_step, ts_prop.bg_per_bin
             ], 0.),
         ("exponential", 2, 1, 3, "fit parameter background 2", "", 
-            [log(ts.bg_per_bin)+2., -1./(0.5*ts.nbins_per_step)], 
-            0.5*ts.nbins_per_step*exp(log(ts.bg_per_bin)+2.)*(
-                exp(-(1.5*ts.nbins_per_step-3.*ts.peak_width*ts.nbins_per_step)/(0.5*ts.nbins_per_step))-
-                exp(-(1.5*ts.nbins_per_step+3.*ts.peak_width*ts.nbins_per_step)/(0.5*ts.nbins_per_step))
+            [log(ts_prop.bg_per_bin)+2., -1./(0.5*ts_prop.nbins_per_step)], 
+            0.5*ts_prop.nbins_per_step*exp(log(ts_prop.bg_per_bin)+2.)*(
+                exp(-(1.5*ts_prop.nbins_per_step-3.*ts_prop.peak_width*ts_prop.nbins_per_step)/(0.5*ts_prop.nbins_per_step))-
+                exp(-(1.5*ts_prop.nbins_per_step+3.*ts_prop.peak_width*ts_prop.nbins_per_step)/(0.5*ts_prop.nbins_per_step))
                 )
             ),
     ]
     )
-def test_backgroundRegions(model, nparams, step, nregions, settings, errormessage, bgparams, bg_volume, temp_file):
-    if not os.path.isfile(ts.filename):
-        raise IOError('Test spectrum was not generated!')
+def test_backgroundRegions(model, nparams, step, nregions, settings, errormessage, bgparams, bg_volume, temp_file, test_spectrum):
 
-    spec_interface.LoadSpectra(ts.filename)
+    spec_interface.LoadSpectra(test_spectrum.filename)
     command  = ['fit function background activate ' + model]
     if settings is not "":
         command.append(settings)
     for i in range(nregions):
-        command.append('fit marker background set %f' % ((step+ts.bg_regions[i][0])*ts.nbins_per_step))
-        command.append('fit marker background set %f' % ((step+ts.bg_regions[i][1])*ts.nbins_per_step))
-    command.append('fit marker region set %f' % ((step+0.5)*ts.nbins_per_step - 3.*ts.peak_width*ts.nbins_per_step))
-    command.append('fit marker region set %f' % ((step+0.5)*ts.nbins_per_step + 3.*ts.peak_width*ts.nbins_per_step))
-    command.append('fit marker peak set %f' % ((step+0.5)*ts.nbins_per_step))
+        command.append('fit marker background set %f' % ((step+test_spectrum.bg_regions[i][0])*test_spectrum.nbins_per_step))
+        command.append('fit marker background set %f' % ((step+test_spectrum.bg_regions[i][1])*test_spectrum.nbins_per_step))
+    command.append('fit marker region set %f' % ((step+0.5)*test_spectrum.nbins_per_step - 3.*test_spectrum.peak_width*test_spectrum.nbins_per_step))
+    command.append('fit marker region set %f' % ((step+0.5)*test_spectrum.nbins_per_step + 3.*test_spectrum.peak_width*test_spectrum.nbins_per_step))
+    command.append('fit marker peak set %f' % ((step+0.5)*test_spectrum.nbins_per_step))
 
     command.append('fit execute')
     command.append('fit store')
@@ -98,12 +122,13 @@ def test_backgroundRegions(model, nparams, step, nregions, settings, errormessag
     command.append('fit marker peak delete 0')
     f, ferr = hdtvcmd(*command)
 
-    batchfile = os.path.join(os.path.curdir, 'test', 'share', model + '_' + str(nparams) + '_' + str(step) + '_background.hdtv')
-    bfile = open(batchfile, 'w')
-    bfile.write('spectrum get '+ ts.filename + '\n')
-    for c in command:
-        bfile.write(c + '\n')
-    bfile.close()
+    if WRITE_BATCHFILE:
+        batchfile = os.path.join(os.path.curdir, 'test', 'share', model + '_' + str(nparams) + '_' + str(step) + '_background.hdtv')
+        bfile = open(batchfile, 'w')
+        bfile.write('spectrum get '+ test_spectrum.filename + '\n')
+        for c in command:
+            bfile.write(c + '\n')
+        bfile.close()
 
     if errormessage is not '':
         hdtvcmd('fit delete 0', 'spectrum delete 0')
@@ -127,8 +152,8 @@ def test_backgroundRegions(model, nparams, step, nregions, settings, errormessag
         bgMarkers = fits[0].findall('bgMarker')
         assert len(bgMarkers) == nregions
         for i in range(len(bgMarkers)):
-            assert isclose(float(bgMarkers[i].find('begin').find('cal').text), (step+ts.bg_regions[i][0])*ts.nbins_per_step, abs_tol=BG_MARKER_TOLERANCE)
-            assert isclose(float(bgMarkers[i].find('end').find('cal').text), (step+ts.bg_regions[i][1])*ts.nbins_per_step, abs_tol=BG_MARKER_TOLERANCE)
+            assert isclose(float(bgMarkers[i].find('begin').find('cal').text), (step+test_spectrum.bg_regions[i][0])*test_spectrum.nbins_per_step, abs_tol=BG_MARKER_TOLERANCE)
+            assert isclose(float(bgMarkers[i].find('end').find('cal').text), (step+test_spectrum.bg_regions[i][1])*test_spectrum.nbins_per_step, abs_tol=BG_MARKER_TOLERANCE)
         # Check the number (and values) of background parameters
         background = fits[0].find('background')
         assert background.get('backgroundModel') == model
@@ -143,8 +168,8 @@ def test_backgroundRegions(model, nparams, step, nregions, settings, errormessag
 
         # Check the fit result
         # All results will be compared 
-        assert isclose(float(fits[0].find('peak').find('cal').find('vol').find('value').text), ts.peak_volume, abs_tol=N_SIGMA*float(fits[0].find('peak').find('cal').find('vol').find('error').text)) 
-        assert isclose(float(fits[0].find('peak').find('cal').find('width').find('value').text), SIGMA_TO_FWHM*ts.peak_width*ts.nbins_per_step, abs_tol=N_SIGMA*float(fits[0].find('peak').find('cal').find('width').find('error').text)) 
+        assert isclose(float(fits[0].find('peak').find('cal').find('vol').find('value').text), test_spectrum.peak_volume, abs_tol=N_SIGMA*float(fits[0].find('peak').find('cal').find('vol').find('error').text)) 
+        assert isclose(float(fits[0].find('peak').find('cal').find('width').find('value').text), SIGMA_TO_FWHM*test_spectrum.peak_width*test_spectrum.nbins_per_step, abs_tol=N_SIGMA*float(fits[0].find('peak').find('cal').find('width').find('error').text)) 
 
         # Check the integration result
         integrals = fits[0].findall('integral')
@@ -154,9 +179,9 @@ def test_backgroundRegions(model, nparams, step, nregions, settings, errormessag
         assert integrals[1].get('integraltype') == 'bg'
         assert integrals[2].get('integraltype') == 'sub'
         # Peak volume
-        assert isclose(float(integrals[2].find('uncal').find('vol').find('value').text), ts.peak_volume, abs_tol=N_SIGMA*float(integrals[2].find('uncal').find('vol').find('error').text))
+        assert isclose(float(integrals[2].find('uncal').find('vol').find('value').text), test_spectrum.peak_volume, abs_tol=N_SIGMA*float(integrals[2].find('uncal').find('vol').find('error').text))
         # Background volume
         if bg_volume > 0.:
             assert  isclose(float(integrals[1].find('uncal').find('vol').find('value').text), bg_volume, abs_tol=sqrt(bg_volume))
         else:
-            assert isclose(float(integrals[1].find('uncal').find('vol').find('value').text), ts.bg_per_bin*6.*ts.peak_width*ts.nbins_per_step, abs_tol=N_SIGMA*sqrt(ts.bg_per_bin*6.*ts.peak_width*ts.nbins_per_step))
+            assert isclose(float(integrals[1].find('uncal').find('vol').find('value').text), test_spectrum.bg_per_bin*6.*test_spectrum.peak_width*test_spectrum.nbins_per_step, abs_tol=N_SIGMA*sqrt(test_spectrum.bg_per_bin*6.*test_spectrum.peak_width*test_spectrum.nbins_per_step))
