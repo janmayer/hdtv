janmayer/hdtv

commit da303fd89f5146386b577914fb47a50fe8828445

Fixed wrong cut axis from mfile matrices

hdtv/histogram.py
@@ -624,11 +624,11 @@ class MHisto2D(Histo2D):
                 othercal = self._yproj.cal
             else:
                 othercal = self._xproj.cal
-            matrix = self.vmatrix
+            matrix = self.tvmatrix
         else:
             thiscal = self._yproj.cal
             othercal = self._xproj.cal
-            matrix = self.tvmatrix
+            matrix = self.vmatrix
 
         matrix.ResetRegions()
 

commit 36738db85b2cfe66de0c1d581cb7e12804d76180

Hotfix none-type issues with symmetric matrices introduced with da303fd89f5146386b577914fb47a50fe8828445

hdtv/histogram.py
@@ -679,7 +679,7 @@ class MHisto2D(Histo2D):
 
         if sym:
             self._yproj = None
-            self.tvmatrix = None
+            self.tvmatrix = self.vmatrix  # Fixme
         else:
             self._yproj = FileHistogram(basename + ".pry")
             self._yproj.typeStr = "Projection"