12608ee4370127a26bcc99cce4d38aa1cf69cb1b
Mayr, Hannes <hmayr@ikp.tu-darmstadt.de>
2024-05-03 14:01:08 +0200

    Switch to alternative copyright variable to avoid overwriting builtins.

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -8,22 +8,22 @@ https://www.sphinx-doc.org/en/master/usage/configuration.html
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'hdtv'
-copyright = '2024, IKP'
-author = 'IKP'
+project = "hdtv"
+project_copyright = "2024, IKP"
+author = "IKP"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['autoapi.extension']
-autoapi_dirs = ['../../hdtv']
+extensions = ["autoapi.extension"]
+autoapi_dirs = ["../../hdtv"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
