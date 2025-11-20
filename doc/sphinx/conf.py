806442c1e5e5b8c368f7f787dd5b2b91043d16a4
Thomas Heijligen <thomas.heijligen@stud.tu-darmstadt.de>
2025-11-20 20:57:32 +0100

    Follow python src-layout
    Move the hdtv package to src and follow the python src-layout convention.
    Fix all paths in doc.
    
    https://setuptools.pypa.io/en/latest/userguide/package_discovery.html#src-layout

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -16,7 +16,7 @@ author = "IKP"
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ["autoapi.extension"]
-autoapi_dirs = ["../../hdtv"]
+autoapi_dirs = ["../../src/hdtv"]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
