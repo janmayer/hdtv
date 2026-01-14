0546bef8166a38e8f55de4a1a3e2f0fb17241738
Thomas Heijligen <thomas.heijligen@stud.tu-darmstadt.de>
2026-01-14 12:30:38 +0100

    rootext: Reimplement MatOp in pure C++
    MatOp.cc implements projection and transposing of mfile based matrices
    with the help of functions derived from the matop package.
    
    Reimplement those two functions directly in C++ to reduce code size and
    complexity. Furthermore, this helps to prepare the rootext code to be
    loaded via the ROOT CLING interpreter.
    
    The new implementation was A/B tested against the original implementation
    with the matrices in src/hdtv/rootext/mfile-root/mfile/spec-samples:
    1k.hf4, 1k.hf8, 1k.lf4, 1k.lf8, 1k.txt.
    
    hdtv> matrix get asym src/hdtv/rootext/mfile-root/mfile/spec-samples/1k.*
    
    The generated files were the same.
    The transpose of 1k.vaxf and 1k.vaxg failed both, before and after this
    patch.

--- src/hdtv/rootext/mfile-root/MatOp.cc
+++ src/hdtv/rootext/mfile-root/MatOp.cc
@@ -1,6 +1,6 @@
 /*
  * HDTV - A ROOT-based spectrum analysis software
- *  Copyright (C) 2006-2010  The HDTV development team (see file AUTHORS)
+ *  Copyright (C) 2006-2026  The HDTV development team (see file AUTHORS)
  *
  * This file is part of HDTV.
  *
@@ -22,12 +22,11 @@
 
 #include "MatOp.hh"
 
-#include <iostream>
+#include <algorithm>
+#include <cstdint>
+#include <memory>
 
-#include "MFileRoot.hh"
-#include "matop/matop_adjust.h"
-#include "matop/matop_conv.h"
-#include "matop/matop_project.h"
+#include <mfile.h>
 
 const int MatOp::ERR_SUCCESS = 0;
 const int MatOp::ERR_UNKNOWN = 1;
@@ -56,85 +55,285 @@ const char *MatOp::ErrDesc[] = {
     "Transposition failed"                          // ERR_TRANS_FAIL
 };
 
-int MatOp::Project(const char *src_fname, const char *prx_fname, const char *pry_fname) {
-  if (prx_fname && !(*prx_fname)) {
-    prx_fname = nullptr;
-  }
-  if (pry_fname && !(*pry_fname)) {
-    pry_fname = nullptr;
-  }
-
-  MFile in_matrix(src_fname, "r");
-  if (in_matrix.IsZombie()) {
-    return ERR_SRC_OPEN;
-  }
-
-  // std::cout << "Info: input format is " << mgetfmt(in_matrix, NULL) <<
-  // std::endl;
-
-  MFile out_prx(prx_fname, "w");
-  if (out_prx.IsZombie()) {
-    return ERR_PRX_OPEN;
-  }
-
-  if (!out_prx.IsNull()) {
-    if (matop_adjustfmts_prx(static_cast<MFILE *>(out_prx), static_cast<MFILE *>(in_matrix)) != 0) {
-      return ERR_PRX_FMT;
-    }
-  }
-
-  MFile out_pry(pry_fname, "w");
-  if (out_pry.IsZombie()) {
-    return ERR_PRY_OPEN;
-  }
-
-  if (!out_pry.IsNull()) {
-    if (matop_adjustfmts_pry(static_cast<MFILE *>(out_pry), static_cast<MFILE *>(in_matrix)) != 0) {
-      return ERR_PRY_FMT;
-    }
-  }
-
-  if (matop_proj(static_cast<MFILE *>(out_prx), static_cast<MFILE *>(out_pry), static_cast<MFILE *>(in_matrix)) != 0) {
-    return ERR_PROJ_FAIL;
-  }
-
-  return ERR_SUCCESS;
-}
-
-int MatOp::Transpose(const char *src_fname, const char *dst_fname) {
-  MFile in_matrix(src_fname, "r");
-  if (in_matrix.IsZombie()) {
-    return ERR_SRC_OPEN;
-  }
-
-  // std::cout << "Info: input format is " << mgetfmt(in_matrix, NULL) <<
-  // std::endl;
-
-  MFile out_matrix(dst_fname, "w");
-  if (out_matrix.IsZombie()) {
-    return ERR_TRANS_OPEN;
-  }
-
-  if (matop_adjustfmts_trans(static_cast<MFILE *>(out_matrix), static_cast<MFILE *>(in_matrix)) != 0) {
-    return ERR_TRANS_FMT;
-  }
-
-  // std::cout << "Info: output format is " << mgetfmt(out_matrix, NULL) <<
-  // std::endl;
-
-  if (matop_conv(static_cast<MFILE *>(out_matrix), static_cast<MFILE *>(in_matrix), MAT_TRANS) != 0) {
-    return ERR_TRANS_FAIL;
-  }
-
-  matop_conv_free_cache();
-
-  return ERR_SUCCESS;
-}
-
 const char *MatOp::GetErrorString(int error_nr) {
   if (error_nr < 0 || error_nr > MAX_ERR) {
     error_nr = ERR_UNKNOWN;
   }
 
   return ErrDesc[error_nr];
+}
+
+namespace {
+template <typename T,
+          // Using mget_ & mput_ to distinguish from definitions of mfile.h
+          int32_t (*mget_)(MFILE *, T *, int32_t, int32_t, int32_t, int32_t),
+          int32_t (*mput_)(MFILE *, T *, int32_t, int32_t, int32_t, int32_t)>
+int ProjectGeneric(MFILE *dstx, MFILE *dsty, unsigned int level, MFILE *src) {
+  minfo info;
+  mgetinfo(src, &info);
+  if (level >= info.levels) {
+    return MatOp::ERR_PROJ_FAIL;
+  }
+
+  auto lbuf = std::make_unique<T[]>(info.columns);
+  auto prx = dstx ? std::make_unique<T[]>(info.columns) : nullptr;
+  auto pry = dsty ? std::make_unique<T[]>(info.lines) : nullptr;
+
+  for (uint32_t l = 0; l < info.lines; l++) {
+    if (static_cast<uint32_t>(mget_(src, lbuf.get(), level, l, 0, info.columns)) != info.columns) {
+      return MatOp::ERR_PROJ_FAIL;
+    }
+
+    if (prx) {
+      for (uint32_t c = 0; prx && c < info.columns; c++) {
+        prx[c] += lbuf[c];
+      }
+    }
+
+    if (pry) {
+      T sum = 0;
+      for (uint32_t c = 0; c < info.columns; c++) {
+        sum += lbuf[c];
+      }
+      pry[l] += sum;
+    }
+  }
+
+  int err = MatOp::ERR_SUCCESS;
+  if (prx && static_cast<uint32_t>(mput_(dstx, prx.get(), level, 0, 0, info.columns)) != info.columns) {
+    err = MatOp::ERR_PROJ_FAIL;
+  }
+
+  if (pry && static_cast<uint32_t>(mput_(dsty, pry.get(), level, 0, 0, info.lines)) != info.lines) {
+    err = MatOp::ERR_PROJ_FAIL;
+  }
+  return err;
+}
+} // namespace
+
+int MatOp::Project(const char *src_fname, const char *prx_fname, const char *pry_fname) {
+  int err = ERR_SUCCESS;
+
+  MFILE *src_matrix = nullptr;
+  MFILE *prx_matrix = nullptr;
+  MFILE *pry_matrix = nullptr;
+
+  src_matrix = mopen(src_fname, "r");
+  if (src_matrix == nullptr) {
+    err = ERR_SRC_OPEN;
+    goto exit;
+  }
+
+  minfo src_info;
+  mgetinfo(src_matrix, &src_info);
+  if (src_info.levels > 2) {
+    err = ERR_PROJ_FAIL;
+    goto exit;
+  }
+
+  if (prx_fname != nullptr && *prx_fname != '\0') {
+    prx_matrix = mopen(prx_fname, "w");
+    if (prx_matrix == nullptr) {
+      err = ERR_PRX_OPEN;
+      goto exit;
+    }
+
+    minfo prx_info;
+    mgetinfo(prx_matrix, &prx_info);
+
+    prx_info.levels = src_info.levels;
+    prx_info.lines = 1;
+    prx_info.columns = src_info.columns;
+
+    if (msetinfo(prx_matrix, &prx_info)) {
+      err = ERR_PRX_FMT;
+      goto exit;
+    }
+  }
+
+  if (pry_fname != nullptr && *pry_fname != '\0') {
+    pry_matrix = mopen(pry_fname, "w");
+    if (pry_matrix == nullptr) {
+      err = ERR_PRY_OPEN;
+      goto exit;
+    }
+
+    minfo pry_info;
+    mgetinfo(pry_matrix, &pry_info);
+
+    pry_info.levels = src_info.levels;
+    pry_info.lines = 1;
+    pry_info.columns = src_info.lines;
+
+    if (msetinfo(pry_matrix, &pry_info)) {
+      err = ERR_PRY_FMT;
+      goto exit;
+    }
+  }
+
+  // Projection of level 0 and 1. src_info.levels is <= 2
+  for (uint32_t l = 0; l < src_info.levels; l++) {
+    switch (src_matrix->filetype) {
+    case MAT_LE2:
+    case MAT_LE4:
+    case MAT_HE2:
+    case MAT_HE4:
+
+    case MAT_LE2T:
+    case MAT_LE4T:
+    case MAT_HE2T:
+    case MAT_HE4T:
+
+    case MAT_SHM:
+    case MAT_LC:
+    case MAT_MATE:
+    case MAT_TRIXI:
+      err = ProjectGeneric<int, mgetint, mputint>(prx_matrix, pry_matrix, l, src_matrix);
+      break;
+    case MAT_LF4:
+    case MAT_HF4:
+    case MAT_VAXF:
+      err = ProjectGeneric<float, mgetflt, mputflt>(prx_matrix, pry_matrix, l, src_matrix);
+      break;
+    case MAT_LF8:
+    case MAT_HF8:
+    case MAT_VAXG:
+    case MAT_TXT:
+      err = ProjectGeneric<double, mgetdbl, mputdbl>(prx_matrix, pry_matrix, l, src_matrix);
+      break;
+    default:
+      err = ERR_PROJ_FAIL;
+      goto exit;
+    }
+
+    if (err != ERR_SUCCESS) {
+      break;
+    }
+  }
+
+exit:
+  // using mclose on a nullptr has no effect
+  mclose(src_matrix);
+  mclose(prx_matrix);
+  mclose(pry_matrix);
+
+  return err;
+}
+
+namespace {
+// This function is implemented in this way because the default matrix type of libmfile is MAT_LC,
+// which is a line-compressed format. This format does not allow writes to random positions.
+// Only (sequential) writes of full lines are possible. Otherwise the matrix file will contain garbage.
+template <typename T,
+          // Using mget_ & mput_ to distinguish from definitions of mfile.h
+          int32_t (*mget_)(MFILE *, T *, int32_t, int32_t, int32_t, int32_t),
+          int32_t (*mput_)(MFILE *, T *, int32_t, int32_t, int32_t, int32_t)>
+int TransposeGeneric(MFILE *dst_matrix, MFILE *src_matrix) {
+  minfo info;
+  mgetinfo(src_matrix, &info);
+
+  constexpr int32_t MAX_BUFFER_ELEMENTS = 16 * 1024 * 1024 / sizeof(T);
+
+  int32_t buffered_dst_columns = std::max(1, MAX_BUFFER_ELEMENTS / static_cast<int32_t>(info.lines));
+  auto src_buffer = std::make_unique<T[]>(buffered_dst_columns);
+  auto dst_buffer = std::make_unique<T[]>(buffered_dst_columns * info.lines);
+
+  for (int32_t level = 0; level < static_cast<int32_t>(info.levels); level++) {
+    for (int32_t column = 0; column < static_cast<int32_t>(info.columns); column += buffered_dst_columns) {
+
+      int32_t cc = std::min(buffered_dst_columns, static_cast<int32_t>(info.columns) - column);
+
+      for (int32_t line = 0; line < static_cast<int32_t>(info.lines); line++) {
+        if (mget_(src_matrix, &src_buffer[0], level, line, column, cc) != cc) {
+          return MatOp::ERR_TRANS_FAIL;
+        }
+        for (auto c = 0; c < buffered_dst_columns; c++) {
+          dst_buffer[(c * info.lines) + line] = src_buffer[c];
+        }
+      }
+
+      for (auto c = 0; c < cc; c++) {
+        if (mput_(dst_matrix, &dst_buffer[c * info.lines], level, column + c, 0, info.lines) !=
+            static_cast<int32_t>(info.lines)) {
+          return MatOp::ERR_TRANS_FAIL;
+        }
+      }
+    }
+  }
+
+  return MatOp::ERR_SUCCESS;
+}
+} // namespace
+
+int MatOp::Transpose(const char *src_fname, const char *dst_fname) {
+  int err = ERR_SUCCESS;
+
+  // All MFILE* are initialized to have defined behavior when exiting early
+  MFILE *src_matrix = nullptr;
+  MFILE *dst_matrix = nullptr;
+
+  src_matrix = mopen(src_fname, "r");
+  if (src_matrix == nullptr) {
+    err = ERR_SRC_OPEN;
+    goto exit;
+  }
+
+  dst_matrix = mopen(dst_fname, "w");
+  if (dst_matrix == nullptr) {
+    err = ERR_TRANS_OPEN;
+    goto exit;
+  }
+
+  minfo src_info;
+  minfo dst_info;
+
+  mgetinfo(src_matrix, &src_info);
+  mgetinfo(dst_matrix, &dst_info);
+
+  dst_info.levels = src_info.levels;
+  dst_info.lines = src_info.columns;
+  dst_info.columns = src_info.lines;
+  if (msetinfo(dst_matrix, &dst_info)) {
+    err = ERR_TRANS_FMT;
+    goto exit;
+  }
+
+  switch (src_matrix->filetype) {
+  case MAT_LE2:
+  case MAT_LE4:
+  case MAT_HE2:
+  case MAT_HE4:
+
+  case MAT_LE2T:
+  case MAT_LE4T:
+  case MAT_HE2T:
+  case MAT_HE4T:
+
+  case MAT_SHM:
+  case MAT_LC:
+  case MAT_MATE:
+  case MAT_TRIXI:
+    err = TransposeGeneric<int, mgetint, mputint>(dst_matrix, src_matrix);
+    break;
+  case MAT_LF4:
+  case MAT_HF4:
+  case MAT_VAXF:
+    err = TransposeGeneric<float, mgetflt, mputflt>(dst_matrix, src_matrix);
+    break;
+  case MAT_LF8:
+  case MAT_HF8:
+  case MAT_VAXG:
+  case MAT_TXT:
+    err = TransposeGeneric<double, mgetdbl, mputdbl>(dst_matrix, src_matrix);
+    break;
+  default:
+    err = ERR_TRANS_FAIL;
+  }
+
+exit:
+  // using mclose on a nullpointer has no effect
+  mclose(src_matrix);
+  mclose(dst_matrix);
+
+  return err;
 }
